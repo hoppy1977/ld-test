06529e2aea40330d745f13139e3da4b7b7cc9789
Shane Hopcroft <shane.hopcroft@recordpoint.com>
2021-01-15 18:12:25 +1100

    Upload initial attempt at test app

---
+++ ld-test.py
@@ -0,0 +1,14 @@
+import ldclient
+from ldclient.config import Config
+
+LD_SDK_KEY='<TODO: Insert LD SDK key here>'
+FLAG_KEY='<TODO: Insert LD flag key here>'
+
+ldclient.set_config(Config(LD_SDK_KEY))
+ld_client = ldclient.get()
+
+show_feature = ld_client.variation(FLAG_KEY, {"key": "user@test.com"}, False)
+print(show_feature)
+
+#ldclient.get().flush()
+ldclient.get().close()
